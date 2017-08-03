4cd081bb2a34401fed72df3261ee0459e6161df5
Estudiante <anibalgamboac@gmail.com>
2017-08-03 12:21:43 -0500

    para impares

--- taller11.c
+++ taller11.c
@@ -33,19 +33,19 @@ void * funcion_hilo(void *arg){
 
 	int i = 0;
 
-	int *suma = (int *)malloc (sizeof(int));
+	long *suma = (long *)malloc (sizeof(long));
 	for(i = argumentos->inicio; i <= argumentos->final; i++){
 		
-		suma += argumentos->arreglo[i];
+		*suma += argumentos->arreglo[i];
 	}
+	printf ("La suma de este hilo es %ld\n",*suma);
 	
-	return (void *)suma;
+	return (void *) *suma;
 
 }
 
 int main (int argc, char *argv[]){
-	double time1 = obtenerTiempoActual();
-	srand(time(0));
+	
 	if (argc!=3){
 		printf("No se ingreso bien los parametros");
 	}
@@ -63,33 +63,60 @@ int main (int argc, char *argv[]){
 		arreglo[i]=num;
 
 	}
+	
+	double time1 = obtenerTiempoActual();
+	srand(time(0));
 	pthread_t hilos[num_hilos];
 	int j;
 	int razon = size/num_hilos;
 	int v=0;
-	for (j=0;j<num_hilos;j++){
-		estructura *s_hilo = malloc(sizeof(estructura));
-		s_hilo -> arreglo=arreglo;
-		s_hilo -> inicio=v;
-		v += razon;
-		s_hilo -> final=v-1; 
-		int status=pthread_create(&hilos[j],NULL,funcion_hilo,(void*)s_hilo);
-		if(status<0){
-			fprintf(stderr,"Error al crear el hilo");
-				}
+	if (size % num_hilos == 0){
+		for (j=0;j<num_hilos;j++){
+			estructura *s_hilo = malloc(sizeof(estructura));
+			s_hilo -> arreglo=arreglo;
+			s_hilo -> inicio=v;
+			v += razon;
+			s_hilo -> final=v-1; 
+			int status=pthread_create(&hilos[j],NULL,funcion_hilo,(void*)s_hilo);
+			if(status<0){
+				fprintf(stderr,"Error al crear el hilo");
+			}
 		
+		}
+	}
+	else{
+		for (j=0;j<num_hilos;j++){
+			estructura *s_hilo = malloc(sizeof(estructura));
+			s_hilo -> arreglo=arreglo;
+			s_hilo -> inicio=v;
+			v += razon;
+			if(v>size){
+				s_hilo -> final=size;
+			}else{
+				s_hilo -> final=v; 
+			}
+			v += 1;
+			int status=pthread_create(&hilos[j],NULL,funcion_hilo,(void*)s_hilo);
+			if(status<0){
+				fprintf(stderr,"Error al crear el hilo");
+			}
+		
+		}
 	}
 
 	int h;
-	int total=0;
+	long total=0;
 	for (h=0;h<num_hilos;h++){
 		void *retorno = NULL;
-		int status1= pthread_join(hilos[h],&retorno);
-		total += (long)retorno;
+		pthread_join(hilos[h],&retorno);
+		
+		total += (long) retorno;
+		
+		
 		
 
 	}
-	printf ("La suma total es %d",total);
+	printf ("La suma total es %ld\n",total);
 	double time2 = obtenerTiempoActual();
 	double final= time2-time1;
 	printf ("El tiempo final de ejecuccion es %.9f\n",final);
